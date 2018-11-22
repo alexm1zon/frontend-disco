7b45b84123db3423cceb24d629d3e932ffd09ac2
Alexandre Mizon <amizon@Alexandres-MacBook-Pro.local>
2018-11-21 20:06:46 -0500

    Add account screen

--- app/screens/accounts.js
+++ app/screens/accounts.js
@@ -1,17 +1,65 @@
 import React, { Component } from 'react';
 import {
+  AsyncStorage,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
 export default class Accounts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+       chequingAccountNumber: "64136167171",
+       chequingBalance: '1581.13',
+       savingAccountNumber: "95181167171",
+       savingBalance: '9861.79',
+    };
+  }
+
+  async componentWillMount() {
+    const chequingBalance = await AsyncStorage.getItem('chequingBalance');
+    const savingBalance = await AsyncStorage.getItem('savingBalance');
+    if (chequingBalance) {
+      this.setState({
+        chequingBalance: chequingBalance,
+      });
+    }
+    if (savingBalance) {
+      this.setState({
+        savingBalance: savingBalance,
+      });
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>
-          Account
-        </Text>
+        <View style={{height: 50, backgroundColor: '#2aa202'}}>
+          <Text style={styles.title}>
+            Chequing
+          </Text>
+        </View>
+        <View style={{backgroundColor: 'white'}}>
+          <Text style={styles.subtitle}>
+            Account Number: {this.state.chequingAccountNumber}
+          </Text>
+          <Text style={styles.subtitle}>
+            Balance: {this.state.chequingBalance}$
+          </Text>
+        </View>
+        <View style={{height: 50, backgroundColor: '#2aa202'}}>
+          <Text style={styles.title}>
+            Savings
+          </Text>
+        </View>
+        <View style={{backgroundColor: 'white'}}>
+          <Text style={styles.subtitle}>
+            Account Number: {this.state.savingAccountNumber}
+          </Text>
+          <Text style={styles.subtitle}>
+            Balance: {this.state.savingBalance}$
+          </Text>
+        </View>
       </View>
     );
   }
@@ -20,13 +68,19 @@ export default class Accounts extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    alignItems: 'stretch',
   },
   title: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  }
+    color: 'white',
+  },
+  subtitle: {
+    fontSize: 20,
+    textAlign: 'left',
+    margin: 10,
+    color: 'black',
+  },
 });
--- app/screens/home.js
+++ app/screens/home.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import {
+  Button,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
 export default class Home extends Component {
+  viewAccounts () {
+    console.log('My Accounts');
+  }
+  viewTransfers () {
+    console.log('My Transfers');
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
-          Home
+          Welcome
         </Text>
+        <Button style={styles.button}
+          onPress={this.viewAccounts.bind(this)}
+          title="Accounts"
+          titleStyle={{ fontWeight: "700" }}
+          buttonStyle={{
+            backgroundColor: "rgba(92, 99,216, 1)",
+            width: 300,
+            height: 45,
+            borderColor: "transparent",
+            borderWidth: 0,
+            borderRadius: 5
+          }}
+          containerStyle={{ marginTop: 20 }}
+        />
+        <Button style={styles.button}
+          onPress={this.viewTransfers.bind(this)}
+          title="Transfers"
+          titleStyle={{ fontWeight: "700" }}
+          buttonStyle={{
+            backgroundColor: "rgba(92, 99,216, 1)",
+            width: 300,
+            height: 45,
+            borderColor: "transparent",
+            borderWidth: 0,
+            borderRadius: 5
+          }}
+          containerStyle={{ marginTop: 20 }}
+        />
       </View>
     );
   }
@@ -25,8 +60,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   title: {
-    fontSize: 20,
+    fontSize: 40,
+    textAlign: 'center'
+  },
+  button: {
+    color: 'white',
     textAlign: 'center',
-    margin: 10,
+    fontSize: 20,
+
   }
 });
